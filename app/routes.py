7a90e84405f4d4a8b1b5d300517e0498a033e299
2084 <potexin2084@gmail.com>
2020-07-01 15:03:26 +0500

    логика генерации + комменты

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from app import app, check_url, generate_url as gen
 from flask import render_template, request, url_for, redirect, abort
 
 our_domain = 'http://localhost:5000/'
-link_size = 6
+link_size = 5
 #our_domain = 'http://' +"The_best_domain_in_the_world/"
 @app.route('/')
 @app.route('/make_short', methods = ["POST", "GET"])
@@ -10,18 +10,31 @@ def make_short():
     #если пользователь нажал на кнопку
     if request.method == "POST":
         #если кнопка сгенерировать
-        if request.form['submit_button'] == "Random generate":
-            user_url=our_domain+gen.generate_url(link_size) #TODO:генерируем ссылку
+        if request.form['submit_button'] == "Random generate": # нажатие кнопки сгенерировать рандомныую ссылку
+            new_link_size = link_size
+            while(True):
+                user_url=our_domain+gen.generate_url(new_link_size) #генерируем ссылку
+                if not check_url.check_url(user_url):# проверка ссылки
+                    new_link_size += 1 # увеличиваем длинну ссылки для большего кол-ва вариантов 
+                    user_url=our_domain+gen.generate_url(new_link_size)#генерируем новую ссылку
+                else:
+                    break
             #TODO: добавляем в БД
-            return render_template("main_page.html", user_url=user_url)
+            # в БД закидывать request.form["text_field"]   как начальная ссылка пользователя 
+            # user_url как сгенерированная ссылка
+            return render_template("main_page.html", user_url=user_url)#вывод рандомной короткой ссылки для копирования
         #если кнопка Готово
-        elif request.form['submit_button'] == 'Get URL!':
-            user_url = request.form["text_field"]  # получить инфу из поля
-            # проверка ссылки
-            if not check_url.check_url(user_url):
+        elif request.form['submit_button'] == 'Get URL!': # нажатие кнопки  ссылку
+            user_url = our_domain + request.form["text_field"] #TODO: заменить text_field на имя tb из которого берем 
+            #пользовательскую короткую ссылку  # получить инфу из поля ввода короткой пользовательской ссылки 
+            if not check_url.check_url(user_url):# проверка ссылки
                 abort(400)    # кидаем ошибку 404
             else:
-                return render_template("main_page.html", user_url=user_url)
+                #TODO: добавляем в БД
+                # в БД закидывать request.form["text_field"]   как начальная ссылка пользователя
+                # user_url как пользовательская короткая ссылка
+                return render_template("main_page.html", user_url=user_url) #вывод пользовательсокй короткой ссылки для копирования
+           
         else:
             pass # unknown
     #если метод "Get"
